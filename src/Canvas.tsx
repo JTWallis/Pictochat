4212343b0b8cce6875adb06d97fcd336ceea818d
Justin <wallis.justin123@gmail.com>
2025-09-14 18:23:13 +0200

    feat: pass username to created Message

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -53,7 +53,7 @@ function Canvas(props: any) {
         y: 0
     });
 
-    const [message, setMessage] = useState(new Message([]));
+    const [message, setMessage] = useState(new Message([], props.username));
 
 
     useImperativeHandle(props.canvasRef, () => ({
@@ -106,7 +106,7 @@ function Canvas(props: any) {
         props.addMessage(message);
         await postMessage(message);
         clearCanvas();
-        setMessage(new Message([]));
+        setMessage(new Message([], props.username));
     }
 
     /**
