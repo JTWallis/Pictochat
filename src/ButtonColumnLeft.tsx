806caee6a685af316630d0e48a296c64cadf086a
Justin <wallis.justin123@gmail.com>
2025-09-15 11:50:42 +0200

    feat: added toolbox images

--- src/ButtonColumnLeft.tsx
+++ src/ButtonColumnLeft.tsx
@@ -1,28 +1,38 @@
 import './ButtonColumnLeft.css'
 
-import ImagePen from './assets/img_button_pen.png';
+import ImgScrollUp from './assets/img_button_toolbox_scroll_up.png';
+import ImgScrollDown from './assets/img_button_toolbox_scroll_down.png';
+import ImagePenDraw from './assets/img_button_toolbox_pen_draw.png';
+import ImagePenErase from './assets/img_button_toolbox_pen_erase.png';
+import ImagePenBig from './assets/img_button_toolbox_pen_big.png';
+import ImagePenSmall from './assets/img_button_toolbox_pen_small.png';
+import ImageMapLatin from './assets/img_button_toolbox_charmap_latin.png';
+import ImageMapAccent from './assets/img_button_toolbox_charmap_accent.png';
+import ImageMapJapan from './assets/img_button_toolbox_charmap_japanese.png';
+import ImageMapSpecial from './assets/img_button_toolbox_charmap_special.png';
+import ImageMapPicto from './assets/img_button_toolbox_charmap_pictochat.png';
 
 function ButtonColumnLeft( {scrollListRef}: any ) {
     return (
         <div className="buttonColumn">
-            <button className="button marginTop borderBot" onClick={() => scrollListRef.current.scrollUp()}></button>
-            <button className="button marginTop borderTop" onClick={() => scrollListRef.current.scrollDown()}></button>
+            <input type="image" className="button marginTop borderBot" src={ImgScrollUp} onClick={() => scrollListRef.current.scrollUp()}></input>
+            <input type="image" className="button marginTop borderTop" src={ImgScrollDown} onClick={() => scrollListRef.current.scrollDown()}></input>
 
             <hr className="borderDotted marginTop marginBot"></hr>
 
-            <input type="image" className="button marginTop" src={ImagePen} />
-            <button className="button marginTop"></button>
+            <input type="image" className="button marginTop" src={ImagePenDraw} />
+            <input type="image" className="button marginTop" src={ImagePenErase} ></input>
             <hr className="borderInvisible marginTop marginBot"></hr>
-            <button className="button marginTop"></button>
-            <button className="button marginTop"></button>
+            <input type="image" className="button marginTop" src={ImagePenBig} ></input>
+            <input type="image" className="button marginTop" src={ImagePenSmall} ></input>
 
             <hr className="borderDotted marginTop marginBot"></hr>
 
-            <button className="button marginTop borderBot roundTopLeft"></button>
-            <button className="button marginTop borderBot roundTopLeft"></button>
-            <button className="button marginTop borderBot roundTopLeft"></button>
-            <button className="button marginTop borderBot roundTopLeft"></button>
-            <button className="button marginTop borderBot roundTopLeft"></button>
+            <input type="image" className="button marginTop borderBot roundTopLeft" src={ImageMapLatin} ></input>
+            <input type="image" className="button marginTop borderBot roundTopLeft" src={ImageMapAccent}></input>
+            <input type="image" className="button marginTop borderBot roundTopLeft" src={ImageMapJapan}></input>
+            <input type="image" className="button marginTop borderBot roundTopLeft" src={ImageMapSpecial}></input>
+            <input type="image" className="button marginTop borderBot roundTopLeft" src={ImageMapPicto}></input>
         </div>
     );
 }
