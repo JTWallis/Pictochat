638860514601476517b533425618b002a2a8bf3f
Justin <wallis.justin123@gmail.com>
2025-09-16 15:14:55 +0200

    refactor: utilizing specific charmap image sources instead of labels

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,29 +1,12 @@
 import { useEffect, useState } from 'react';
 import ImgPen from './assets/img_button_pen.png'
 import './Keyboard.css'
+import { CharmapLatin } from './CharmapLatin';
 
-const keys = [
-    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=",
-    "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "BACKSPACE",
-    "CAPS", "a", "s", "d", "f", "g", "h", "j", "k", "l", "ENTER",
-    "SHIFT", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/",
-    ";", "´", "SPACE", "[", "]"
-];
+const charmapLatin = new CharmapLatin();
 
-function createRowRange(_start: string, _end: string) {
-    return {
-        start: keys.indexOf(_start),
-        end: keys.indexOf(_end)
-    };
-}
-
-const rowOneIndexRange = createRowRange("1", "=");
-const rowTwoIndexRange = createRowRange("q", "BACKSPACE");
-const rowThreeIndexRange = createRowRange("CAPS", "ENTER");
-const rowFourIndexRange = createRowRange("SHIFT", "/");
-const rowFiveIndexRange = createRowRange(";", "]");
-
-const rowIndices = [rowOneIndexRange, rowTwoIndexRange, rowThreeIndexRange, rowFourIndexRange, rowFiveIndexRange];
+let representations = charmapLatin.getCharRepresentations();
+let rowRangeIndices = charmapLatin.getRowRangeIndices();
 
 function Keyboard(props: any) {
     const rows: any = [];
@@ -31,6 +14,8 @@ function Keyboard(props: any) {
     const [buttonTarget, setButtonTarget] = useState<HTMLButtonElement | null>(null);
     const [elem, setElem] = useState<HTMLElement | null>(null);
     const [mouseDown, setMouseDown] = useState(false);
+    const [isUpper, setIsUpper] = useState(false);
+    const [isShift, setIsShift] = useState(false);
 
 
     useEffect(() => {
@@ -72,27 +57,27 @@ function Keyboard(props: any) {
         props.floatingKeyRef.current.setPos(event.pageX, event.pageY);
     }
 
-    for (let k = 0; k < rowIndices.length; k++) {
-        for (let i = rowIndices[k].start; i <= rowIndices[k].end; i++) {
+    for (let k = 0; k < rowRangeIndices.length; k++) {
+        for (let i = rowRangeIndices[k].x; i <= rowRangeIndices[k].y; i++) {
             let span = 1;
 
-            if (keys[i] === "BACKSPACE" || keys[i] === "CAPS") span = 2;
-            else if (keys[i] === "SHIFT" || keys[i] === "ENTER") span = 3;
-            else if (keys[i] === "SPACE") span = 5;
+            if (representations[i].valueLower === "BACK" || representations[i].valueLower === "CAPS") span = 2;
+            else if (representations[i].valueLower === "SHIFT" || representations[i].valueLower === "ENTER") span = 3;
+            else if (representations[i].valueLower === "SPACE") span = 5;
 
             rows.push(
-                <button
+                <input
+                    type="image"
+                    src={isUpper ? representations[i].srcUpper : representations[i].srcLower}
                     key={"Keyboard-Button-" + i}
                     style={{ gridRow: k + 1, gridColumnEnd: "span " + span }}
-                    value={keys[i]}
+                    className="imageButton"
                     onMouseDown={button_mousedown}
                     onClick={e => {
-                        props.onKeyboardButtonClick(keys[i]);
+                        props.onKeyboardButtonClick(representations[i]);
                         e.currentTarget.blur();
                     }}
-                >
-                    {keys[i]}
-                </button>
+                />
             );
         }
     }
