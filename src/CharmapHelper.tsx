7e126eebc4226ea4a0cb158d7750ee994ecb8c85
Justin <wallis.justin123@gmail.com>
2025-10-06 13:35:16 +0200

    feat: added helper function to merge a secondary array elems into first array empty slot

--- src/CharmapHelper.tsx
+++ src/CharmapHelper.tsx
@@ -3,6 +3,14 @@ function countNonEmptySlots(arr: any[]): number {
     return arr.filter(e => e).length;
 }
 
+function mergeArrIntoEmptySlots(src: any[], merge: any[]): any[] {
+    for (let i = 0; i < merge.length; i++) {
+        if (!merge[i]) continue;
+        if (!src[i]) src[i] = merge[i];
+    }
+    return src;
+}
+
 function searchCharReps(reps: CharRepresentation[], values: string[], handledValues: number) {
     const foundCharReps: CharRepresentation[] = Array(values.length);
 
