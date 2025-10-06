da8f5e4b1ac7003502d384f8bd40ab71293a9fae
Justin <wallis.justin123@gmail.com>
2025-10-06 13:36:30 +0200

    feat: added function to find existing CharReps from all passed CharMaps for the passed values

--- src/CharmapHelper.tsx
+++ src/CharmapHelper.tsx
@@ -1,3 +1,7 @@
+import type { CharmapBase } from "./CharmapBase";
+import { CharmapBaseDouble } from "./CharmapBaseDouble";
+import { CharmapBaseSingle } from "./CharmapBaseSingle";
+import type { CharRepresentation } from "./CharRepresentation";
 
 function countNonEmptySlots(arr: any[]): number {
     return arr.filter(e => e).length;
@@ -25,4 +29,35 @@ function searchCharReps(reps: CharRepresentation[], values: string[], handledVal
     };
 
     return foundCharReps;
+}
+
+function findCharRepsFromTextValues(charmaps: CharmapBase[], values: string[]) {
+    let handledValues = 0;
+    let charReps: CharRepresentation[] = Array(values.length);
+
+    for (let charmap of charmaps) {
+        if (charmap instanceof CharmapBaseSingle) {
+            const reps = (charmap as CharmapBaseSingle).getCharRepresentations();
+            const foundCharReps = searchCharReps(reps, values, handledValues);
+            charReps = mergeArrIntoEmptySlots(charReps, foundCharReps);
+            handledValues = countNonEmptySlots(charReps);
+
+        } else if (charmap instanceof CharmapBaseDouble) {
+            const reps = (charmap as CharmapBaseDouble).getCharRepresentations();
+            const upperReps = reps.map(e => e.upper);
+            let foundCharReps = searchCharReps(upperReps, values, handledValues);
+            charReps = mergeArrIntoEmptySlots(charReps, foundCharReps);
+            handledValues = countNonEmptySlots(charReps);
+            if (handledValues >= charReps.length) break;
+
+            const lowerReps = reps.map(e => e.lower);
+            foundCharReps = searchCharReps(lowerReps, values, handledValues);
+            charReps = mergeArrIntoEmptySlots(charReps, foundCharReps);
+            handledValues = countNonEmptySlots(charReps);
+        }
+
+        if (handledValues >= charReps.length) break;
+    };
+
+    return charReps;
 }
