5a98f2d4a70df7532374f2fd8576f76172c9a5fb
Justin <wallis.justin123@gmail.com>
2025-10-08 14:43:15 +0200

    feat: converting received, raw MessageDto into Message and pass it to callback function

--- src/MessageController.tsx
+++ src/MessageController.tsx
@@ -6,6 +6,17 @@ import { clientEstablished, stompClient } from "./StompClient";
 const listenEndpoint = "/topic/room/a/messages";
 
 function onReceivedMessage(message: IMessage, onReceivedMessageCallback: (msg: Message) => void) {
+    let receivedDto;
+    try {
+        const rawDto = JSON.parse(message.body) as MessageDto;
+        receivedDto = MessageDto.createFromRaw(rawDto);
+    } catch(e) {
+        console.error("Error when parsing JSON of MessageDto:", e);
+        return;
+    }
+
+    const msg = new Message(receivedDto.commands, receivedDto.creatorName);
+    onReceivedMessageCallback(msg);
 }
 
 export function subscribeMessages(onReceivedMessageCallback: (msg: Message) => void) {
@@ -20,7 +31,7 @@ export async function postMessage(message: Message) {
     if(!clientEstablished()) return;
 
     const msgDto = new MessageDto(message.getCommands(), message.getUsername());
-
+    
     stompClient.publish({
         destination: "/app/messages",
         body: JSON.stringify(msgDto)
