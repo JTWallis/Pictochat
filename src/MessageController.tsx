3ffaebb608c5985763ef4ae9109015389f3e70d7
Justin <wallis.justin123@gmail.com>
2025-10-08 14:39:28 +0200

    feat: added function to subscribe to messages of a room with a callback function for handling new Messages

--- src/MessageController.tsx
+++ src/MessageController.tsx
@@ -1,13 +1,26 @@
+import type { IMessage } from "@stomp/stompjs";
 import { Message } from "./Message";
 import { MessageDto } from "./MessageDto";
 import { clientEstablished, stompClient } from "./StompClient";
 
+const listenEndpoint = "/topic/room/a/messages";
+
+function onReceivedMessage(message: IMessage, onReceivedMessageCallback: (msg: Message) => void) {
+}
+
+export function subscribeMessages(onReceivedMessageCallback: (msg: Message) => void) {
+    if(!clientEstablished()) return;
+
+    stompClient.subscribe(listenEndpoint, (message) => {
+        onReceivedMessage(message, onReceivedMessageCallback);
+    });
+}
 
 export async function postMessage(message: Message) {
     if(!clientEstablished()) return;
 
     const msgDto = new MessageDto(message.getCommands(), message.getUsername());
-    
+
     stompClient.publish({
         destination: "/app/messages",
         body: JSON.stringify(msgDto)
