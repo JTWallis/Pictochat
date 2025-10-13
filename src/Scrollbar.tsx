637966c5c0379d5c16a8b5ad8e02a270abdb233b
jwHybris <wallis.justin123@gmail.com>
2025-10-13 09:57:52 +0200

    fix: negative overflowUp when maxDisplayElems exceeds segmentCount

--- src/Scrollbar.tsx
+++ src/Scrollbar.tsx
@@ -66,8 +66,10 @@ function Scrollbar( {scrollbarRef}: any ) {
     function scrollReset() {
         setSelectedIndex(0);
         setOverflowDownCount(0);
-        const overflowUp = maxDisplayElements > 0 ? (segmentCount - maxDisplayElements) : 0;
-        setOverflowUpCount(overflowUp);
+        const segmentDisplayDiff = segmentCount - maxDisplayElements;
+        if(maxDisplayElements > 0 && segmentDisplayDiff > 0) {
+            setOverflowUpCount(segmentDisplayDiff);
+        }
     }
 
     function scrollUp() {
