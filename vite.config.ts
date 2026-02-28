94d4867aa17e20698cabe27a8dc3a8507a8d766c
Justin Wallis <wallis.justin123@gmail.com>
2026-02-28 13:53:01 +0100

    feat: added alias import for root packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@assets':      path.resolve(__dirname, 'src/assets'),
+      '@components':  path.resolve(__dirname, 'src/components'),
+      '@contexts':    path.resolve(__dirname, 'src/contexts'),
+      '@enums':       path.resolve(__dirname, 'src/enums'),
+      '@models':      path.resolve(__dirname, 'src/models'),
+      '@services':    path.resolve(__dirname, 'src/services'),
+      '@utils':       path.resolve(__dirname, 'src/utils')
+    }
+  }
 })
