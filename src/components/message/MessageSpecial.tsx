d8373fa6fca729a4b3d2eb9d11498a41fd3c99cc
Justin-Windows <wallis.justin123@gmail.com>
2026-07-03 14:53:32 +0200

    refactor: made props type-safe (overlooked in #153)

--- src/components/message/MessageSpecial.tsx
+++ src/components/message/MessageSpecial.tsx
@@ -3,20 +3,30 @@ import { CanvasTypes, type CanvasSpecialPartialAPI } from './canvas/CanvasAPI';
 import type { Vector2 } from '@models/Vector2';
 import { useContext } from 'react';
 import { ThemeContext } from '@contexts/ThemeContext';
+import type { Message } from '@models/Message';
+import type { CharRepresentation } from '@models/charrepresentations/CharRepresentation';
 
 const INIT_HEIGHT_PERCENT = 50;
 
-function MessageSpecial( {message, messageText, textColor, findCharRepFromValue, convertTextToCharReps}: any ) {
+export type MessageSpecialProps = {
+    message: Message,
+    messageText: string,
+    textColor: string,
+    findCharRepFromValue: (value: string) => CharRepresentation | undefined;
+    convertTextToCharReps: (text: string) => CharRepresentation[];
+}
+
+function MessageSpecial(props: MessageSpecialProps) {
 
     const api: CanvasSpecialPartialAPI = {
-        convertTextToCharReps,
+        convertTextToCharReps: props.convertTextToCharReps,
         pushMessageCommand
     }
 
     const theme = useContext(ThemeContext);
 
     function pushMessageCommand(type: number, startPos: Vector2, endPos: Vector2, value: string, penSize: number, penColor: string) {
-        message.pushCommand(type, startPos, endPos, value, penSize, penColor);
+        props.message.pushCommand(type, startPos, endPos, value, penSize, penColor);
     }
 
     return (
@@ -24,12 +34,12 @@ function MessageSpecial( {message, messageText, textColor, findCharRepFromValue,
             defaultHeightPercent={INIT_HEIGHT_PERCENT}
             backgroundColor={theme.canvas_special}
             canvasType={CanvasTypes.CANVAS_SPECIAL}
-            message={message} 
-            findCharRepFromValue={findCharRepFromValue}
+            message={props.message} 
+            findCharRepFromValue={props.findCharRepFromValue}
             hideName={true}
             specialProperties={{
-                messageText: messageText,
-                textColor,
+                messageText: props.messageText,
+                textColor: props.textColor,
                 api: api
             }}
         />
