d889f3b88c8cbeb9e71d2d5708f58527faac48f1
Justin-Windows <wallis.justin123@gmail.com>
2026-07-03 11:45:13 +0200

    refactor: made props type-safe

--- src/components/buttoncolumnright/ButtonColumnRight.tsx
+++ src/components/buttoncolumnright/ButtonColumnRight.tsx
@@ -2,14 +2,19 @@ import './ButtonColumnRight.css';
 import ImageSend from '@assets/img_button_large_send.png';
 import ImageCopy from '@assets/img_button_large_copy.png';
 import ImageDiscard from '@assets/img_button_large_discard.png';
+import type { CanvasSketchHandle } from '@components/message/canvas/sketch/CanvasSketch';
 
-function ButtonColumnRight( {canvasSketchRef}: any ) {
+type ButtonColumnRightProps = {
+    canvasSketchRef: React.RefObject<CanvasSketchHandle | null>;
+}
+
+function ButtonColumnRight(props: ButtonColumnRightProps ) {
 
     return(
         <div className="buttonColumnRight backgroundDark">
-            <input type="image" className="buttonLarge buttonSend" src={ImageSend} onClick={() => canvasSketchRef.current.sendMessage()} />
-            <input type="image" className="buttonLarge buttonCopy" src={ImageCopy} onClick={() => canvasSketchRef.current.copyMessage()} />
-            <input type="image" className="buttonLarge buttonDiscard" src={ImageDiscard} onClick={() => canvasSketchRef.current.discardMessage()} />
+            <input type="image" className="buttonLarge buttonSend" src={ImageSend} onClick={() => props.canvasSketchRef.current!.sendMessage()} />
+            <input type="image" className="buttonLarge buttonCopy" src={ImageCopy} onClick={() => props.canvasSketchRef.current!.copyMessage()} />
+            <input type="image" className="buttonLarge buttonDiscard" src={ImageDiscard} onClick={() => props.canvasSketchRef.current!.discardMessage()} />
         </div>
     );
 }
